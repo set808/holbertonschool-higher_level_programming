592ce5e176aef3d77f29f90819820614a2c7420f
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-10 19:08:02 +0000

    Add 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str) :
+    new = ""
+    for i in str :
+        char = ord(i)
+        if 97 <= char <= 122 :
+            char -= 32
+        new += chr(char)
+    print(new)
