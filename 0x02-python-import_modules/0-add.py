e5a52c5b75d82fc5ef0ee125fa3049e8ef494ce5
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-12 19:14:21 +0000

    Add 0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from add_0 import add
+
+
+def print_add():
+    a = 1
+    b = 2
+
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    print_add()
