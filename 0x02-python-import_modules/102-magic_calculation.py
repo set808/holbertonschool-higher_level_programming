b44e7c77754b20fc9fa8755e9e2abdb58d29611b
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-12 22:32:49 +0000

    Correct 102-magic_calculation.py pep8 errors

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,6 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
-    
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
@@ -10,7 +9,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-
-if __name__ == '__main__':
-    import dis
-    dis.dis(magic_calculation)
