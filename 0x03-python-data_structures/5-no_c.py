807c54428332c78f8482bd4642df558dfabaf21c
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-17 19:31:18 +0000

    Correct 5-no_c.py pep8 error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     my_list = list(my_string)
     for i, char in enumerate(my_list):
-        if (char == 'c' or char ==  'C'):
+        if (char == 'c' or char == 'C'):
             del my_list[i]
     return ''.join(my_list)
