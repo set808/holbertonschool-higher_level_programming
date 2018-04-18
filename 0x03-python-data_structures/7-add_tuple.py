efaa3e7a602e004f311e7ce92c78ad742fa9347a
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-18 01:37:48 +0000

    Create 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+def tuple_check(t):
+    length = len(t)
+    if length == 0:
+        new = (0, 0)
+        return new
+    elif length == 1:
+        new = (t[0], 0)
+        return new
+    return t
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_check(tuple_a)
+    tuple_b = tuple_check(tuple_b)
+    return tuple(map(sum, zip(tuple_a, tuple_b)))
