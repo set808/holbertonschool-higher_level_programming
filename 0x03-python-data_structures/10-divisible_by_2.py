bb4d4c0319269442bbe0c7bb5028f62eefbcaaa8
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-18 02:19:55 +0000

    Create 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+def divisible_by_2(my_list=[]):
+    return tuple(map(is_even, my_list))
