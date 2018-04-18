d24a1aba76155c186784edb13a40e0a304b6a481
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-18 02:38:58 +0000

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        max_int = 0
-        for i, num in enumerate(my_list):
+        max_int = my_list[0]
+        for i, num in enumerate(my_list[1:]):
             if num > max_int:
                 max_int = num
         return max_int
