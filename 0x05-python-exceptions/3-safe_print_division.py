dae61942a5ef8c4cd8d767fe40d2340940461200
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-30 20:17:37 +0000

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
+    result = None
     try:
         result = a / b
     except ZeroDivisionError:
-        return 
+        return
     finally:
         print('Inside result: {}'.format(result))
     return result
