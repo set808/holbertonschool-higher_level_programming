4bf3a49adaa312b0d1693ca29e9913c81fd2d980
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-30 21:29:39 +0000

    Create 102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def main():
+    result = 0;
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            resutl = a + b
+            break
+    return result
