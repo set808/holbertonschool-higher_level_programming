206033b578193d49b417ba0279e38b14956dbced
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-30 20:07:45 +0000

    Add NULL check and count variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        try:
-            print(my_list[i], end='')
-        except IndexError:
-            print()
-            return (i)
+    count = 0;
+    if my_list:
+        for i in range(x):
+            try:
+                print(my_list[i], end='')
+            except IndexError:
+                print()
+                return (i)
+            else:
+                count += 1
     print()
     return (i + 1)
