5ef8cbc5e2659fc7d852e2749cea2a2225182b93
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-30 20:09:51 +0000

    Update 0-safe_print-list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end='')
             except IndexError:
                 print()
-                return (i)
+                return (count)
             else:
                 count += 1
     print()
-    return (i + 1)
+    return (count)
