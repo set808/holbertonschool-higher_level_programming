96014ba311cff9a788fb82baa696252a9b98be3d
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-30 20:35:37 +0000

    Move return True statment

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
+        return True
     except ValueError as err:
         sys.stderr.write('Exception: {}\n'.format(err))
         return False
-    return True
