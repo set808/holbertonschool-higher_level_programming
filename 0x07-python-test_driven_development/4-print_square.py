39e7b1f4d21146d8e941b57a6eaa8d7fc542e5a0
Spencer Taylor <spencertaylor808@gmail.com>
2018-05-03 22:05:52 +0000

    Create 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_square(size):
+    if isinstance(size, int) is False:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for i in range(size):
+        print('{:s}'.format('#' * size))
