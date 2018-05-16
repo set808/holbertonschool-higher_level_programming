ff64fb1136bdd151f5b8e7c8131cab0df48d4ba8
Spencer Taylor <spencertaylor808@gmail.com>
2018-05-16 04:53:02 +0000

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Defines a class BaseGeometry
+'''
+
+class BaseGeometry:
+    '''BaseGeometry class
+
+    '''
+    def area(self):
+        '''defines area function
+
+        '''
+        raise Exception('area() is not implemented')
