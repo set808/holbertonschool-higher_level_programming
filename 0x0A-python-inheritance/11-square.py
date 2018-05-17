83e949dbeb6f2f9ef851119b79e08a109b2dda12
Spencer Taylor <spencertaylor808@gmail.com>
2018-05-17 23:25:31 +0000

    Correct inheritance in multiple files

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,58 +2,7 @@
 '''
 Defines classes BaseGeometry, Rectangle, and Square
 '''
-
-
-class BaseGeometry:
-    '''BaseGeometry class
-
-    '''
-
-    def area(self):
-        '''defines area function
-
-        '''
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        '''Validates value
-
-        Args:
-            name (str): the name of something
-            value (int): some value that wasn't explained
-        '''
-
-        if not isinstance(value, int):
-            raise TypeError('{:s} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{:s} must be greater than 0'.format(name))
-
-
-class Rectangle(BaseGeometry):
-    ''' Rectangle class inherits from BaseGeometry
-
-    '''
-
-    def __init__(self, width, height):
-        """Initializes a Rectangle object.
-        Args:
-            width (int): Width of the rectangle.
-            height (int): Height of the rectangle.
-        """
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        return '[{:s}] {:d}/{:d}'.format(type(self).__name__,
-                                         self.__width, self.__height)
-
-    def area(self):
-        '''defines area function.
-
-        '''
-        return self.__height * self.__width
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,31 +2,7 @@
 '''
 Defines two classes BaseGeometry and Rectangle
 '''
-
-
-class BaseGeometry:
-    '''BaseGeometry class
-
-    '''
-
-    def area(self):
-        '''defines area function
-
-        '''
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        '''Validates value
-
-        Args:
-            name (str): the name of something
-            value (int): some value that wasn't explained
-        '''
-
-        if not isinstance(value, int):
-            raise TypeError('{:s} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{:s} must be greater than 0'.format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
