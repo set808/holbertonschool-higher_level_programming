740c6a244e561fbc181845b476b08666a32cb36b
Spencer Taylor <spencertaylor808@gmail.com>
2018-05-17 02:11:23 +0000

    Create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Defines a class Student
+'''
+
+
+class Student:
+    '''Student class
+
+
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''Initializes a Student object
+
+        Attributes:
+            first_name (str): first name of Student
+            last_name (str): last name of Student
+            age (int): age of Student
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''Retrieves a dictionary representation of instance
+
+        '''
+        return self.__dict__
