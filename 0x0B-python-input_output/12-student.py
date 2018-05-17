336a2a1c9b489b1feb32dab7c80373be373bd22a
Spencer Taylor <spencertaylor808@gmail.com>
2018-05-17 02:12:33 +0000

    Create 12-student.py

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Defines a class Student
+'''
+
+
+class Student:
+    '''Student class
+
+
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''Initializes a Student object
+
+        Attributes:
+            first_name (str): first name of Student
+            last_name (str): last name of Student
+            age (int): age of Student
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Retrieves a dictionary representation of instance
+
+        '''
+        if attrs and \
+           isinstance(attrs, list) is True and\
+           all(isinstance(x, str) for x in attrs) is True:
+            return {key: self.__dict__[key]
+                    for key in attrs if key in self.__dict__}
+        return self.__dict__
