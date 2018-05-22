972d0d2d44dff6a540bb318d22319968ee157fae
Spencer Taylor <spencertaylor808@gmail.com>
2018-05-22 17:32:16 +0000

    Update for pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''
 Defines a class Base
 '''
+import json
 
 
 class Base:
@@ -21,3 +22,68 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries ([[]]): a list of dictionaries
+
+        Return:
+            returns the JSON representation of list_dictionaries or \
+            "[]" if list_dictionaries is None or empty
+        '''
+        return json.dumps(list_dictionaries or [])
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Returns the list of the JSON string representation
+
+        Args:
+            json_string (str): the JSON string
+
+        Return:
+            returns a list of JSON strings
+        '''
+        return json.loads(json_string or [])
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Returns an instance with all atrributes setattr
+
+        Args:
+            dictionary (**dict): double pointer to a dictionary
+
+        Returns:
+            instance with all attributes set
+        '''
+        new = cls(1, 1)
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Writes JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): list of instances
+        '''
+        try:
+            json_string = cls.to_json_string(
+                [x.to_dictionary() for x in list_objs])
+        except BaseException:
+            json_string = '[]'
+        with open(cls.__name__ + '.json', 'w') as f:
+            f.write(json_string)
+
+    @classmethod
+    def load_from_file(cls):
+        '''Returns a list of instances
+
+        Return:
+            returns a list of instances form a JSON string
+        '''
+        with open(cls.__name__ + '.json', 'r') as f:
+            list_dict = cls.from_json_string(f.read())
+            return [cls.create(**x) for x in list_dict]
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,6 +27,7 @@ class Square(Rectangle):
         '''
         return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(self.id,
                                                          self.x, self.y, self.width)
+
     @property
     def size(self):
         '''Returns the size of the Square object
