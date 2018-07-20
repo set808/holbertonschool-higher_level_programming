dc6774de3010892cb29082f2742faf513ab78574
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-20 03:22:14 +0000

    Rework check functionality

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,10 +12,11 @@ def main(argv):
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
 
     for row in cur.fetchall():
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
 
     db.close()
 
