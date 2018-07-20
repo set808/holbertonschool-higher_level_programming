005f31947138b954cee64cdba696b80f7c166487
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-20 00:14:35 +0000

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Define get states function'''
+import MySQLdb
+
+
+def main(argv):
+    if len(argv) != 4:
+        print("Incorrect number of arguments")
+        return
+
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
+
+    for row in cur.fetchall():
+        print(row)
+
+    db.close()
+
+
+if __name__ == '__main__':
+    from sys import argv
+    main(argv)
