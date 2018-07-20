52168ba5fe1c63752ab2dd5ac21af0f3a41cacf1
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-20 03:25:18 +0000

    Change SQL statement

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,8 @@ def main(argv):
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name \
-    LIKE "{:s}" ORDER BY id ASC'.format(argv[4]))
+    cur.execute('SELECT * FROM states WHERE name  = "{:s}" \
+    ORDER BY id ASC'.format(argv[4]))
 
     for row in cur.fetchall():
         print(row)
