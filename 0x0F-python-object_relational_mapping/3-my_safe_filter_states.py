464c4fe6fc711fac09bc761ba67ed83a6f53ae95
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-20 03:13:33 +0000

    Create 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Function that filters by user input'''
+import MySQLdb
+from sys import argv
+
+
+def main(argv):
+
+    if len(argv) != 5:
+        print('Incorrect number of arguments')
+        return
+
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name=%s', [argv[4]])
+
+    for row in cur.fetchall():
+        print(row)
+
+    db.close()
+
+if __name__ == '__main__':
+    main(argv)
