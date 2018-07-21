920873d255c6fc5480a89ef7363c616051ad681f
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-21 05:55:59 +0000

    Create remaining task files

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""List objects that contain the letter a """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine), update
+from sqlalchemy.orm import sessionmaker
+
+
+def main(argv):
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+    for state in session.query(State).filter(
+            State.name == "Louisiana").order_by(
+            State.id).all():
+
+        print("{}".format(state.id))
+    session.close()
+
+
+
+if __name__ == "__main__":
+    main(argv)
