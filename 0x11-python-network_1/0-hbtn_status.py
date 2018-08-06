ec6500f8859fa0551968962ed27d0e48305eeabd
Spencer Taylor <spencertaylor808@gmail.com>
2018-08-06 20:02:30 +0000

    Fix typo fix from last typo

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ def main():
     with urllib.request.urlopen(req) as response:
         body = response.read()
 
-    print('Body repsonse:')
+    print('Body response:')
     print('\t- type: {}'.format(type(body)))
     print('\t- content: {}'.format(body))
     print('\t- utf8 content: {}'.format(body.decode('utf8')))
