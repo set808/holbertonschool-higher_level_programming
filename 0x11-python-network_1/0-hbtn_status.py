505f84d42024176774235019ead49e4810feedc4
Spencer Taylor <spencertaylor808@gmail.com>
2018-08-06 20:00:49 +0000

    Fix typo in output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@ import urllib.request
 
 
 def main():
-    '''Python Script'''
+
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         body = response.read()
 
-    print('Body reponse:')
+    print('Body repsonse:')
     print('\t- type: {}'.format(type(body)))
     print('\t- content: {}'.format(body))
     print('\t- utf8 content: {}'.format(body.decode('utf8')))
