c55c19f54ab15a8800f41b48dbecfd5224b203e9
Spencer Taylor <spencertaylor808@gmail.com>
2018-08-06 23:06:30 +0000

    Create 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin python3
+'''Defines a script for POST request'''
+import requests
+
+
+def main(args):
+
+    r = requests.post(args[1], data = {'email': args[2]})
+    print(r.text)
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
