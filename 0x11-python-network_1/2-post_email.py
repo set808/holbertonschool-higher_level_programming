2d6bb683c7adfd6e91c731db11990b529cc5461f
Spencer Taylor <spencertaylor808@gmail.com>
2018-08-06 21:45:55 +0000

    Create 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Defines a script to send a POST request'''
+import urllib.parse
+import urllib.request
+
+
+def main(args):
+
+    url = args[1]
+    values = {'email': args[2]}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+
+    print(body.decode('utf8'))
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
