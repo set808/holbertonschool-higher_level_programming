c3b65d7396ff65fa69ce91e15674568c1fc5c750
Spencer Taylor <spencertaylor808@gmail.com>
2018-08-06 21:46:52 +0000

    Create 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Defines script to display the body of response'''
+import urllib
+
+
+def main(args):
+
+    url = args[1]
+    req = urllib.request.Request(url)
+    try:
+        urllib.request.urlopen(req)
+    except urllib.error.URLError as e:
+        print('Error code: {}'.format(e.code))
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
