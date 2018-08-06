39f8e1a0abeb195d2da27e76913fa9199150148e
Spencer Taylor <spencertaylor808@gmail.com>
2018-08-06 19:36:47 +0000

    Create 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Defines a function to fetch a url'''
+import urllib.request
+
+
+def main():
+
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+
+    print('Body reponse:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
+    print('\t- utf8 content: {}'.format(body.decode('utf8')))
+
+
+if __name__ == '__main__':
+    main()
