e66b478fb73355d1f7bbdf1583ffe5f68be3abe4
Spencer Taylor <spencertaylor808@gmail.com>
2018-08-07 00:22:51 +0000

    Create 9-starwars.py

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Sends a search request to Star Wars API'''
+import requests
+
+
+def main(args):
+    r = requests.get("https://swapi.co/api/people/?search=" + args[1])
+    r = r.json()
+    print('Number of results: {}'.format(r['count']))
+    if r['count'] != 0:
+        for result in r['results']:
+            print(result['name'])
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
