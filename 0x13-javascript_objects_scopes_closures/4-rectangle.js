ee558df7d2b1b6962c0d04544e312bd70f659555
Spencer Taylor <spencertaylor808@gmail.com>
2018-09-04 23:08:26 -0700

    Add project files

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if (w  > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        let i;
+        for (i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width))
+        }
+    }
+
+    rotate(){
+        let temp = this.width;
+        this.width = this.height;
+        this.height = temp;
+    }
+
+    double() {
+        this.height *= 2;
+        this.width *= 2;
+    }
+}
